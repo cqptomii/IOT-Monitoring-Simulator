b3dd4323083f1a3a7926405a7df9f938b06f83b4
Tom <tomfraisse.pro@gmail.com>
2024-09-22 00:32:58 +0200

    edit Server Class

--- Server.cpp
+++ Server.cpp
@@ -4,28 +4,25 @@
 
 #include "Server.hpp"
 
-Server::Server() : console_logs(true), file_report(false) {
-    logfile_path = std::filesystem::current_path();
-    std::cout << logfile_path << std::endl;
+Server::Server() : console_logs(true), file_logs(false) {
+    server_log_file = std::filesystem::current_path();
+    std::cout << server_log_file << std::endl;
 }
 
 Server::Server(const Server &server) = default;
 
-Server::Server(bool write_console_logs, bool write_file_report) :console_logs(write_console_logs), file_report(write_file_report) {
-    logfile_path = std::filesystem::current_path();
+Server::Server(bool write_console_logs, bool write_file_report) :console_logs(write_console_logs), file_logs(write_file_report) {
+    server_log_file = std::filesystem::current_path();
+    current_time = time(nullptr);
 }
 
-Server &Server::operator=(const Server &server) {
+Server &Server::operator=(const Server& server) {
     console_logs = server.console_logs;
-    logfile_path = server.logfile_path;
-    file_report = server.file_report;
+    server_log_file = server.server_log_file;
+    file_logs = server.file_logs;
+    current_time = server.current_time;
     return *this;
 }
 
-std::ostream &operator<<(std::ostream &os, const Server &server) {
 
-}
 
-std::ostream &operator>>(std::ostream &os, const Server &server){
-
-}
--- Server.hpp
+++ Server.hpp
@@ -6,36 +6,95 @@
 #define TP_SERVER_HPP
 #include <iostream>
 #include <filesystem>
+#include <fstream>
 #include <ctime>
 #include <string>
 class Server {
 private:
-    std::filesystem::path logfile_path;
-    bool file_report;
+    std::filesystem::path server_log_file;
+    bool file_logs;
     bool console_logs;
+    std::time_t current_time;
 
     /**
      * @brief Fonction permettant de visualiser les données reçues des capteurs dans la console
+     * @param data : Tuple contenant les données à ecrire dans la console de logs
+     */
+    template<typename T>
+    void consoleWrite(const std::tuple<size_t ,std::string,std::string, T>& data){
+        this->writting_format(std::cout,data);
+    }
+
+    /**
+     * @brief Fonction permettant de stockées les données des capteurs dans des fichiers de logs selon le type de capteurs
+     * @param file : fichier dans lequel les données seront ecrites
+     * @param data : Tuple contenant les données à ecrire dans le fichier de logs
+     */
+    template<typename T>
+    void fileWrite(std::ofstream& open_file ,const std::tuple<size_t ,std::string,std::string, T> &data){
+        this->writting_format(open_file,data);
+    }
+
+    /**
+     * @brief write_data
      * @param os :
      * @param data :
      */
     template<typename T>
-    void consoleWrite(std::ostream& os,T data){
-        std::time_t current_time = std::time(nullptr);
+    void writting_format(std::ofstream& os,const std::tuple<size_t ,std::string,std::string, T> &data){
+         current_time = time(nullptr);
+         std::string str_time = ctime(&current_time);
 
-        std::string dt = ctime(&current_time);
+         str_time.pop_back(); // supprime le "\n"
 
-        std::cout << dt << std::endl << std::endl;
-        std::cout << "Sensor : " << "    Value: " << data << std::endl;
+         os << str_time << " " << // current Time
+                   std::get<0>(data) << " " << // Sensor ID
+                   std::get<1>(data) << " " << // Sensor Type
+                   std::get<2>(data) << " " << // Senor name
+                   std::get<3>(data) << std::endl; // current data
     }
+
     /**
-     * @brief Fonction permettant de stockées les données des capteurs dans des fichiers de logs selon le type de capteurs
-     * @param file
-     * @param data
+     * @brief write_data
      */
     template<typename T>
-    void fileWrite(std::ofstream& file,T data){
+    void writeData(const std::tuple<size_t ,std::string,std::string, T> &data){
+        bool file_exist(true);
 
+        if(console_logs){
+            consoleWrite(data);
+        }
+        if(file_logs){
+            // Création du dossier log server si non existant
+            if(!std::filesystem::exists(server_log_file.string())){
+                if(!std::filesystem::create_directories(server_log_file)){
+                    std::cerr << "Erreur lors de la creation du dossier" << std::endl;
+                }
+            }
+
+            std::string sensor_logs_folder = server_log_file.string() + "/" + std::get<1>(data)[0] + "_" + std::get<2>(data);
+            // Creation du fichier log associé au capteur si non existant
+            try {
+                if(!std::filesystem::exists(sensor_logs_folder)){
+                    file_exist = false;
+                }
+                std::ofstream log(sensor_logs_folder);
+
+                log.exceptions(std::ofstream::failbit | std::ofstream::badbit); // active les exceptions failbit / badbit
+                log.open(server_log_file, std::ofstream::out | std::ofstream::app);
+
+                if(!file_exist){
+                    log << "Date ID Type Name Value" << std::endl;
+                }
+                fileWrite(log, data);
+
+                log.close();
+            } catch (const std::ofstream::failure &e) {
+                std::cerr << "Erreur lors de l'ouverture du fichier" << std::endl;
+            }
+
+
+        }
     }
 public:
     /**
@@ -54,6 +113,7 @@ public:
      * @param write_file_report  : permet d'activer la production de rapport texte
      */
     Server(bool write_console_logs, bool write_file_report);
+
     /**
      * @brief Destructeur de la classe Server
      */
@@ -64,8 +124,15 @@ public:
      * @return une reference vers le server copié
      */
     Server& operator=(const Server& server);
-    friend std::ostream& operator<<(std::ostream& os, const Server& server);
-    friend std::ostream& operator>>(std::ostream& os, const Server& server);
+
+    /**
+     * @brief Opérateur d'entrée de données vers le server
+     * @param data
+     **/
+    template<typename T>
+    void operator<<(const std::tuple<size_t ,std::string,std::string, T>& data) {
+        this->writeData(data);
+    }
 };
 
 
