e1ef5fd38f6d7b11dfb4e85821bab3e531172d1b
Tom <tomfraisse.pro@gmail.com>
2024-09-26 14:52:30 +0200

    Implementation of the class Scheduler

--- Scheduler.cpp
+++ Scheduler.cpp
@@ -4,14 +4,154 @@
 
 #include "Scheduler.hpp"
 
-Scheduler::Scheduler() {
+Scheduler::Scheduler() : server_ptr(),l_sensor(),s_sensor(),h_sensor(),t_sensor(),interval(5000),is_running(false), last_update(){}
 
+Scheduler::Scheduler(const Scheduler &scheduler) : interval(scheduler.interval),last_update(scheduler.last_update),is_running(scheduler.is_running) {
+    if(scheduler.server_ptr){
+        this->server_ptr = scheduler.server_ptr;
+    }else{
+        this->server_ptr.reset();
+    }
+    for(const std::unique_ptr<Sound> &tempS : scheduler.s_sensor){
+        this->s_sensor.push_back(std::make_unique<Sound>(*tempS));
+    }
+    for(const std::unique_ptr<Light> &tempL : scheduler.l_sensor){
+        this->l_sensor.push_back(std::make_unique<Light>(*tempL));
+    }
+    for(const std::unique_ptr<Humidity> &tempH : scheduler.h_sensor){
+        this->h_sensor.push_back(std::make_unique<Humidity>(*tempH));
+    }
+    for(const std::unique_ptr<Temperature> &tempT : scheduler.t_sensor){
+        this->t_sensor.push_back(std::make_unique<Temperature>(*tempT));
+    }
 }
 
-Scheduler::Scheduler(const Scheduler &scheduler) {
+Scheduler &Scheduler::operator=(const Scheduler& scheduler){
+    if(this != &scheduler){
+        if(scheduler.server_ptr)
+            this->server_ptr = scheduler.server_ptr;
+        else
+            this->server_ptr.reset();
 
-}
+        this->last_update = scheduler.last_update;
+        this->is_running = scheduler.is_running;
 
-Scheduler &Scheduler::operator=(const Scheduler &scheduler) {
+        for(const std::unique_ptr<Sound> &tempS : scheduler.s_sensor){
+            this->s_sensor.push_back(std::make_unique<Sound>(*tempS));
+        }
+        for(const std::unique_ptr<Light> &tempL : scheduler.l_sensor){
+            this->l_sensor.push_back(std::make_unique<Light>(*tempL));
+        }
+        for(const std::unique_ptr<Humidity> &tempH : scheduler.h_sensor){
+            this->h_sensor.push_back(std::make_unique<Humidity>(*tempH));
+        }
+        for(const std::unique_ptr<Temperature> &tempT : scheduler.t_sensor){
+            this->t_sensor.push_back(std::make_unique<Temperature>(*tempT));
+        }
+    }
     return *this;
+}
+void Scheduler::operator<<(const Server& server) {
+    linkServer(std::make_shared<Server>(server));
+}
+void Scheduler::operator<<(const Light &sensor) {
+    linkSensor(std::make_unique<Light>(sensor));
+}
+void Scheduler::operator<<(const Sound &sensor) {
+    linkSensor(std::make_unique<Sound>(sensor));
+}
+void Scheduler::operator<<(const Humidity &sensor) {
+    linkSensor(std::make_unique<Humidity>(sensor));
+}
+void Scheduler::operator<<(const Temperature &sensor) {
+    linkSensor(std::make_unique<Temperature>(sensor));
+}
+
+void Scheduler::linkServer(const std::shared_ptr<Server>& server) {
+    if(server == nullptr)
+        return;
+    if(server != this->server_ptr){
+        this->server_ptr = server;
+
+    }
+}
+
+void Scheduler::linkSensor(std::unique_ptr<Sound> sound_sensor) {
+    sound_sensor->associed_server = this->server_ptr;
+    this->s_sensor.push_back(std::move(sound_sensor));
+}
+
+void Scheduler::linkSensor(std::unique_ptr<Humidity> hum_sensor) {
+    hum_sensor->associed_server = this->server_ptr;
+    this->h_sensor.push_back(std::move(hum_sensor));
+}
+void Scheduler::linkSensor(std::unique_ptr<Temperature> temp_sensor) {
+    temp_sensor->associed_server = this->server_ptr;
+    this->t_sensor.push_back(std::move(temp_sensor));
+}
+void Scheduler::linkSensor(std::unique_ptr<Light> light_sensor) {
+    light_sensor->associed_server = this->server_ptr;
+    this->l_sensor.push_back(std::move(light_sensor));
+}
+
+void Scheduler::simulation() {
+    if(isReady()){
+        std::cout << "Lecture des valeurs des capteurs" << std::endl;
+        last_update = std::chrono::high_resolution_clock::now();
+        for(const std::unique_ptr<Light> &l : this->l_sensor){
+            l->update();
+        }
+        for(const std::unique_ptr<Sound> &s : this->s_sensor){
+            s->update();
+        }
+        for(const std::unique_ptr<Humidity> &h : this->h_sensor){
+            h->update();
+        }
+        for(const std::unique_ptr<Temperature> &t : this->t_sensor){
+            t->update();
+        }
+    }
+}
+void Scheduler::runTasks() {
+    try {
+        while (is_running) {
+            simulation();
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        }
+    }catch(std::exception& e){
+        std::cerr << "Exception dans le thread - Fin du thread" << std::endl;
+    }
+}
+bool Scheduler::start() {
+    is_running = true;
+    if(!this->server_ptr){
+        std::cerr<< "ERROR - pas de server associe au scheduler" << std::endl;
+        return false;
+    }
+    /*
+    if(is_running){
+        return true;
+    }
+     */
+    runTasks();
+    return true;
+}
+
+void Scheduler::stop() {
+    is_running = false;
+    if(thread_test.joinable()){
+        thread_test.join(); // arrêt du thread
+    }
+}
+
+bool Scheduler::isReady() {
+    std::chrono::time_point<std::chrono::high_resolution_clock > currentTime = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double,std::milli> duration(currentTime - this->last_update);
+
+
+    return (duration.count() >= this->interval);
+}
+
+Scheduler::~Scheduler() {
+    stop();
 }
--- Scheduler.hpp
+++ Scheduler.hpp
@@ -4,10 +4,41 @@
 
 #ifndef TP_SCHEDULER_HPP
 #define TP_SCHEDULER_HPP
-
+#include "Sensor/Sensor.h"
+#include "Sensor/Sound.hpp"
+#include "Sensor/Light.hpp"
+#include "Sensor/Temperature.hpp"
+#include "Sensor/Humidity.hpp"
+#include "Server.hpp"
+#include <memory>
+#include <list>
+#include <thread>
 
 class Scheduler {
 private:
+    std::shared_ptr<Server> server_ptr;
+
+    std::list< std::unique_ptr<Light> > l_sensor;
+    std::list< std::unique_ptr<Humidity> > h_sensor;
+    std::list< std::unique_ptr<Sound> > s_sensor;
+    std::list< std::unique_ptr<Temperature> > t_sensor;
+
+    unsigned int interval;
+    std::chrono::time_point<std::chrono::high_resolution_clock> last_update;
+
+    bool is_running;
+    std::thread thread_test;
+    /**
+     * @brief methode qui organise le sequencement des capteurs. Permet d'appeler la fonction update sur l'ensemble des capteurs enregistré dans le scheduler
+     *
+     */
+    void simulation();
+    void linkServer(const std::shared_ptr<Server>& server);
+    void linkSensor(std::unique_ptr<Sound> sound_sensor);
+    void linkSensor(std::unique_ptr<Humidity> hum_sensor);
+    void linkSensor(std::unique_ptr<Temperature> temp_sensor);
+    void linkSensor(std::unique_ptr<Light> light_sensor);
+    bool isReady();
 public:
     /**
      * @brief Constructeur par default de la classe Scheduler
@@ -15,6 +46,7 @@ public:
     Scheduler();
     /**
      * @brief Constructeur de recopie de la classe Scheduler
+     * effectue une copie profonde des attribut de scheduler
      *
      * @param scheduler : Scheduleur à recopier
      */
@@ -22,13 +54,28 @@ public:
     /**
      * @brief Destructeur de la classe Scheduler
      */
-    ~Scheduler() = default;
+    ~Scheduler();
     /**
-     * @brief Opérateur de copie de la classe Scheduler
+     * @brief Opérateur d'affectation de la classe Scheduler
+     *  effectue une copie profonde des attributs de scheduler
      *
      * @param scheduler : Scheduler à copier
-     */
+       */
     Scheduler& operator=(const Scheduler& scheduler);
+    /**
+     * @brief Suchage opérateur de sortie permettant d'envoyer au scheduler un server
+     * @param server : server transmis au scheduler
+     */
+    void operator<<(const Server& server);
+    void operator<<(const Sound& sensor);
+    void operator<<(const Light& sensor);
+    void operator<<(const Humidity& sensor);
+    void operator<<(const Temperature& sensor);
+
+
+    void runTasks();
+    bool start();
+    void stop();
 };
 
 
